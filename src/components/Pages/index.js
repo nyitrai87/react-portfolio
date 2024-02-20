16e8bafdc2c80a0285fd3d5ad89670fcbe6f8275
Adam Nyitrai <adam.nyitrai87@gmail.com>
2024-02-20 02:36:37 +0100

    added custom 404 and route

--- src/components/Pages/index.js
+++ src/components/Pages/index.js
@@ -1,3 +1,4 @@
+export { default as Custom404 } from './404';
 export { default as Home } from './Home';
 export { default as ProjectGallery } from './ProjectGallery';
 export { default as Contact } from './Contact';
