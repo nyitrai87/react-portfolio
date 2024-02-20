b987f223847face27aa8d5ed557329981ac66c7f
Adam Nyitrai <adam.nyitrai87@gmail.com>
2024-02-20 01:39:15 +0100

    added bootstrap cards for projects

--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -1,20 +1,22 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-function GridExample() {
+function ProjectCard({ project }) {
     return (
         <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Img variant="top" src={project.image} />
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Card.Title>{project.title}</Card.Title>
+                <Card.Text>{project.description}</Card.Text>
+                <a href={project.github}><Button variant="primary">GitHub Repository</Button></a>
+                {() => {
+                    if (project.deployed !== '') {
+                        <a href={project.deployed}><Button variant="primary">Deployed Site</Button></a>
+                    }
+                }}
             </Card.Body>
         </Card>
     );
 }
 
-export default GridExample;
+export default ProjectCard;
