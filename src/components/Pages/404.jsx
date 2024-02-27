82144c4f9c29787f9ec74ea851675f6d62b23b2f
Adam Nyitrai <adam.nyitrai87@gmail.com>
2024-02-27 17:21:33 +0000

    build folder added to be able to deploy project

--- src/components/Pages/404.jsx
+++ src/components/Pages/404.jsx
@@ -1,7 +1,7 @@
 const Custom404 = () => {
     return (
         <>
-            <h1>Nothing to see here. :( Please use the navbar to visit an existing page!</h1>
+            <h1>404 Nothing to see here. Please use the navbar to visit an existing page!</h1>
         </>
     );
 }
