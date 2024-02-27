b21255c460daa52608053bb0e99ae4009926da87
Adam Nyitrai <adam.nyitrai87@gmail.com>
2024-02-27 08:44:51 +0000

    added tech stack to projects, to footer, and my skills are now listed on the home screen

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -6,13 +6,15 @@ import './Home.css'
 
 const Home = () => {
     return (
-        <Row className="d-flex justify-content-center g-4 home-row">
+        <Row className="d-flex justify-content-center g-4 bio-row">
             <Col xs={12} md={8} xl={6}>
                 <Container className="rounded-3 jumbotron-text">
                     <h1 className="display-5 fw-bold">Adam Nyitrai</h1>
                     <h2>Front-End Developer | Crafting Engaging Web Experiences</h2><br />
                     <p className="fs-4">Hello and welcome on my portfolio website! <br /><br />
-                        I am a Front-End Developer with a rich background that ranges from warehousing to technical support. My passion for problem-solving led me to earn a certificate in front-end web development from the edX Skills Bootcamp. Now, I am excited to leverage my skills in HTML, CSS, JavaScript, Bootstrap, React.js, and Node.js to create engaging and responsive web experiences. I am always eager to explore new technologies and strive for continuous improvement. Dive in to discover the projects that showcase my journey and growth in the world of web development!</p>
+                        I am a Front-End Developer with a rich background that ranges from warehousing to technical support. My passion for problem-solving led me to earn a certificate in front-end web development from the edX Skills Bootcamp. Now, I am excited to leverage my skills to create engaging and responsive web experiences. I am always eager to explore new technologies and strive for continuous improvement. Dive in to discover the projects that showcase my journey and growth in the world of web development!</p><br />
+                    <h2>My skills, so far:</h2><br />
+                    <p className="fs-4">HTML | CSS | JavaScript | Bootstrap | React.js | Node.js | jQuery | Jest | APIs | Git | GitHub</p>
                 </Container>
             </Col>
 
--- src/components/Project/index.jsx
+++ src/components/Project/index.jsx
@@ -9,11 +9,16 @@ function ProjectCard({ project }) {
             <Card.Body className="d-flex">
                 <Card.Title text="white">{project.title}</Card.Title>
                 <Card.Text>{project.description}</Card.Text>
-                <div className="buttons-div">
-                    <a href={project.github}><Button className="github-btn" variant="primary">GitHub Repository</Button></a>
-                    {project.deployed !== '' ?
-                        <a href={project.deployed}><Button className="deployed-btn" variant="primary">Deployed Site</Button></a>
-                        : ""}
+                <div className="card-bottom">
+                    <Card.Text className="techs">{project.techs.map((tech, i) => (
+                        <img key={i} src={tech} className="tech-logo"></img>
+                    ))}</Card.Text>
+                    <div className="buttons-div">
+                        <a href={project.github}><Button className="github-btn" variant="primary">GitHub Repository</Button></a>
+                        {project.deployed !== '' ?
+                            <a href={project.deployed}><Button className="deployed-btn" variant="primary">Deployed Site</Button></a>
+                            : ""}
+                    </div>
                 </div>
             </Card.Body>
         </Card>
