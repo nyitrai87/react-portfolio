5e8a63b52741203ce8c1f4ffd417cd1fca1d49df
Adam Nyitrai <adam.nyitrai87@gmail.com>
2024-05-10 04:11:53 +0100

    slightly changed home page structure

--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -7,14 +7,16 @@ const Home = () => {
             <Col xs={12} md={8} xl={6}>
                 <Container className="rounded-3 jumbotron-text">
                     <h1 className="display-5 fw-bold">Adam Nyitrai</h1>
-                    <h3>Front-End Developer | Crafting Engaging Web Experiences</h3><br />
-                    <p className="fs-5">Hello and welcome on my portfolio website! <br /><br />
-                        I am a Front-End Developer with a rich background that ranges from warehousing to technical support. My passion for problem-solving led me to earn a certificate in front-end web development from the edX Skills Bootcamp, with A+ grade. Now, I am excited to leverage my skills to create engaging and responsive web experiences. I am always eager to explore new technologies and strive for continuous improvement. Dive in to discover the projects that showcase my journey and growth in the world of web development!</p><br />
-                    <h2>My skills, so far:</h2>
-                    <p className="fs-5">HTML | CSS | JavaScript | Bootstrap | React.js | Node.js | jQuery | Jest | APIs | Firebase | Firestore | Git | GitHub</p>
+                    <h4>Front-End Developer | Crafting Engaging Web Experiences</h4><br />
+                    <p className="fs-6">Hello and welcome on my portfolio website! <br /><br />
+                        I am a Front-End Developer with a rich background that ranges from warehousing to technical support. My passion for problem-solving led me to earn a certificate in front-end web development from the edX Skills Bootcamp, with A+ grade. Now, I am excited to leverage my skills to create engaging and responsive web experiences. I am always eager to explore new technologies and strive for continuous improvement. Dive in to discover the projects that showcase my journey and growth in the world of web development!</p>
+                </Container>
+                <Container className="rounded-3 jumbotron-text mt-4">
+                    <h5 id="skills">Skills, so far:</h5>
+                    <p className="fs-6">HTML | CSS | JavaScript | Bootstrap | React.js | Node.js | jQuery | Jest | APIs | Firebase | Firestore | Git | GitHub</p>
                 </Container>
                 <Container className="rounded-3 jumbotron-img mt-4 img-col">
-                    <h5 id="grade">My Certificate</h5>
+                    <h5 className="cont-header">Certificate</h5>
                     <Image className="rounded-bottom-3 cont-img" src="./images/certificate.png" alt="" />
                 </Container>
             </Col>
@@ -24,7 +26,7 @@ const Home = () => {
                     <Image className="rounded-3 cont-img" src="./images/headshot.png" alt="" />
                 </Container>
                 <Container className="rounded-3 jumbotron-img mt-4">
-                    <h5 id="grade">My Bootcamp Grade</h5>
+                    <h5 className="cont-header">Bootcamp Grade</h5>
                     <Image className="rounded-bottom-3 cont-img" src="./images/bootcamp.jpeg" alt="" />
                 </Container>
             </Col>
